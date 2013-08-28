613f70250702dbad530719d5e6afce97f0982660
Joe Lam <manchun.lam@oracle.com>
2013-08-28 17:48:33 -0400

    [NEW] ItemsController - added create, and json-response template

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,12 @@
 class ItemsController < ApplicationController
+  respond_to :json
+
   def index
   end
 
   def create
+    @item = Item.create(params[:item])
+    respond_with(@item)
   end
 
   def show
